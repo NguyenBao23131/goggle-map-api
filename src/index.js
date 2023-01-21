7e16b168d766abe08c7f585818ff19eb508410b4
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-21 19:28:20 +0700

    update-add-some-log

--- src/index.js
+++ src/index.js
@@ -25,3 +25,5 @@ const arr2 = ['L', 'O', 'T', 'O', 'J', 'O', 'I', 'N'];
 const arr3 = arr1.concat(arr2);
 
 console.log(arr3);
+
+console.log(`Your browser version is reported as ${navigator.appVersion}`);
